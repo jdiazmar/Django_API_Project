85e98f5118c3c4ba1e010c592faae17d1d74c4c5
Josmar Diaz <jdm.begreat28@gmail.com>
2022-05-26 18:32:40 -0400

    Created Products Class In Models File

--- products/views.py
+++ products/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.db import models
 
 # Create your views here.
+
+class Products(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    price = models.DecimalField(max_length=2)
+    inventory_quantity = models.IntegerField(max_length=8)
+
+    
