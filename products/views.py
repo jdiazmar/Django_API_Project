ab5e13aca52bbecd9b6735870f7e953ef71f93df
Josmar Diaz <jdm.begreat28@gmail.com>
2022-05-26 18:36:11 -0400

    Updated Class In Models File

--- products/views.py
+++ products/views.py
@@ -5,7 +5,7 @@ from django.db import models
 class Products(models.Model):
     title = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
-    price = models.DecimalField(max_length=2)
-    inventory_quantity = models.IntegerField(max_length=8)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    inventory_quantity = models.IntegerField()
 
-    
+
