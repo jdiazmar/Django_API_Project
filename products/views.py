74a3d0b71983c5833877d2ff4febda448d45ebc3
Josmar Diaz <jdm.begreat28@gmail.com>
2022-05-26 19:42:04 -0400

    Updated Models File and Views File

--- products/views.py
+++ products/views.py
@@ -2,10 +2,7 @@ from django.db import models
 
 # Create your views here.
 
-class Products(models.Model):
-    title = models.CharField(max_length=255)
-    description = models.CharField(max_length=255)
-    price = models.DecimalField(max_digits=5, decimal_places=2)
-    inventory_quantity = models.IntegerField()
+
+
 
 
