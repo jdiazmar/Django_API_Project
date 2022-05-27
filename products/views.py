69407731be85d6b00b3cbbf04132d09cc0621b6d
Josmar Diaz <jdm.begreat28@gmail.com>
2022-05-26 22:38:42 -0400

    Completed Delete Functionality For Products View File

--- products/views.py
+++ products/views.py
@@ -23,7 +23,7 @@ def products_list(request):
         return Response(serializer.errors, status=status.HTTP_201_CREATED)
 
 
-@api_view(['GET', 'PUT'])
+@api_view(['GET', 'PUT', 'DELETE'])
 def product_detail(request, pk):
     products = get_object_or_404(Products, pk=pk)
     if request.method == 'GET':
@@ -34,3 +34,7 @@ def product_detail(request, pk):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
+    elif request.method == 'DELETE':
+        products.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
